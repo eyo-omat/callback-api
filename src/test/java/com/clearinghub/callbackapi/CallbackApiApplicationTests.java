612af69d820b680c627bf2eb244da68235717be5
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-04-06 23:24:45 +0000

    :clown_face: Added Main files

---
+++ src/test/java/com/clearinghub/callbackapi/CallbackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearinghub.callbackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallbackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
