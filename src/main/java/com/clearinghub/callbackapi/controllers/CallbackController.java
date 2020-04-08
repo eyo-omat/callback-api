8104a0f3ad6a48d6656ca9d300e1b5b749528b43
Eyioyo Omatsola <eyo.omat@gmail.com>
2020-04-08 13:47:00 +0000

    :clown_face: Added functionality to receive incoming messages on the controller

--- src/main/java/com/clearinghub/callbackapi/controllers/CallbackController.java
+++ src/main/java/com/clearinghub/callbackapi/controllers/CallbackController.java
@@ -1,10 +1,16 @@
 package com.clearinghub.callbackapi.controllers;
 
 import com.clearinghub.callbackapi.dtos.responses.ResponseArray;
+import com.clearinghub.callbackapi.services.CallbackServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -17,11 +23,27 @@ import java.util.Map;
  * Time: 10:52 PM
  */
 @RestController
-
+@RequiredArgsConstructor
 public class CallbackController {
 
-    @PostMapping("/incoming_message")
-    public ResponseEntity<ResponseArray> acceptMessage(@RequestBody Map<String, String> body){
+    @NonNull
+    private final CallbackServiceImpl callbackService;
+
+    @PostMapping("/incoming")
+    public ResponseEntity<ResponseArray> receiveMessageCallback(@RequestBody Map<String, String> messageBody) throws JsonProcessingException {
+        callbackService.receiveTwilioCallback(messageBody);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/incoming_message", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseArray> receiveTwilioMessageCallback(@RequestParam Map<String, String> messageBody) throws JsonProcessingException {
+        callbackService.receiveTwilioCallback(messageBody);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/status_callback")
+    public ResponseEntity<ResponseArray> receiveStatusCallback(@RequestBody Map<String, String> statusBody) {
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
